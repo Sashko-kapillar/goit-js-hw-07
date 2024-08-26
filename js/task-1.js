69839367620b20e7fcd2ce131e1d65d6fefa03a2
sashko-kapillar <kapillarua@gmail.com>
2024-08-26 09:05:44 +0300

    update task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,8 @@
-"use strict"
+"use strict";
+
+const listWithId = document.querySelector("#categories");
+listWithId.style.listStyleType = "none";
+listWithId.style.color = "#2e2f42";
+listWithId.style.flexDirection = "column";
+listWithId.style.gap = "24px";
+// console.log(listWithId);
