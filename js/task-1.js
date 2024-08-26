24075202ca70b0a66df707365043d3ede71f585e
sashko-kapillar <kapillarua@gmail.com>
2024-08-26 10:26:57 +0300

    task-1 ok

--- js/task-1.js
+++ js/task-1.js
@@ -6,3 +6,13 @@ listWithId.style.color = "#2e2f42";
 listWithId.style.flexDirection = "column";
 listWithId.style.gap = "24px";
 // console.log(listWithId);
+
+const item = document.querySelectorAll(".item");
+console.log(item.length);
+
+const heading = document.querySelectorAll("h2");
+// console.log(heading);
+heading.forEach((head) => console.log(head.textContent));
+
+const itemLi = document.querySelectorAll(".item li");
+console.log(itemLi.length);
