c576da872ea0e0acac774d7dae3466665c879e3f
sashko-kapillar <kapillarua@gmail.com>
2024-08-26 17:41:22 +0300

    fix

--- js/task-1.js
+++ js/task-1.js
@@ -4,6 +4,8 @@ const listWithId = document.querySelector("#categories");
 listWithId.style.listStyleType = "none";
 listWithId.style.color = "#2e2f42";
 listWithId.style.flexDirection = "column";
+listWithId.style.padding = "24px";
+listWithId.style.width = "440px";
 listWithId.style.gap = "24px";
 
 const item = document.querySelectorAll(".item");
