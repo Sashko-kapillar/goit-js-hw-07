a7e9be07c63b6bcb7fe0cc3d058868d54ecbbd9d
sashko-kapillar <kapillarua@gmail.com>
2024-08-26 17:20:46 +0300

    task-1 fix

--- js/task-1.js
+++ js/task-1.js
@@ -5,14 +5,13 @@ listWithId.style.listStyleType = "none";
 listWithId.style.color = "#2e2f42";
 listWithId.style.flexDirection = "column";
 listWithId.style.gap = "24px";
-// console.log(listWithId);
 
 const item = document.querySelectorAll(".item");
-console.log(item.length);
+console.log("Numbers of categories: ", item.length);
 
-const heading = document.querySelectorAll("h2");
-// console.log(heading);
-heading.forEach((head) => console.log(head.textContent));
-
-const itemLi = document.querySelectorAll(".item li");
-console.log(itemLi.length);
+item.forEach((head) => {
+  const heading = head.querySelector("h2").textContent;
+  console.log(`Category:  ${heading}`);
+  const elem = head.querySelectorAll("ul > li").length;
+  console.log(`Elements:  ${elem}`);
+});
